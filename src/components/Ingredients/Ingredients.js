2778f3158d4836d28126e2bac1558b77d48f227b
VivienIngrams <vivingrams@gmail.com>
2023-07-03 16:30:23 +0100

    Fetching data from firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
@@ -6,6 +6,28 @@ import IngredientList from "./IngredientList";
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://react-tasks-a94be-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log('Rendering', userIngredients);
+  }, [userIngredients])
 
   const addIngredientHandler = (ingredient) => {
     fetch(
@@ -15,14 +37,16 @@ function Ingredients() {
         body: JSON.stringify(ingredient),
         headers: { "Content-Type": "application/json" },
       }
-    ).then((response) => {
-      return response.json();
-    }).then(responseData => {
-      setUserIngredients((prevIngredients) => [
-        ...prevIngredients,
-        { id: responseData.name, ...ingredient },
-      ]);
-    });
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   const removeIngredientHandler = (ingredientId) => {
